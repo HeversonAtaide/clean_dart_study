8d07ffa3ff54d31f12371f7750b9d5785f8fdcdc
heverson.ataide <heverson.ataide@waydatasolution.com>
2021-01-15 19:19:21 -0300

    Criação do projeto com slidy

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:clean_dart_study/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() => runApp(ModularApp(module: AppModule()));
